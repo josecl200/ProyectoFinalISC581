d65ba052a25587f1d2f43a41f10df5b6f3c91798
José C. Ureña <20160138@ce.pucmm.edu.do>
2020-11-08 15:39:16 -0400

    Login delay simulation

--- app/src/main/java/edu/pucmm/isc581/applogin/activities/LoginActivity.java
+++ app/src/main/java/edu/pucmm/isc581/applogin/activities/LoginActivity.java
@@ -1,19 +1,24 @@
 package edu.pucmm.isc581.applogin.activities;
 
+import android.content.Context;
 import android.content.Intent;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Handler;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.pucmm.isc581.applogin.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText email, password;
     Button loginButton;
     TextView forgotPasswordButton, registerButton;
+    ProgressBar loginBulto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +30,24 @@ public class LoginActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.loginButton);
         forgotPasswordButton = findViewById(R.id.forgotPasswordTextView);
         registerButton = findViewById(R.id.registerTextView);
+        loginBulto = findViewById(R.id.progressBar);
 
         loginButton.setOnClickListener(v -> {
+            InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            manager.hideSoftInputFromWindow(v.getWindowToken(), 0);
             if (!email.getText().toString().trim().isEmpty() && !password.getText().toString().trim().isEmpty()){
+                loginBulto.setVisibility(View.VISIBLE);
                 Intent i = new Intent(v.getContext(), MainActivity.class);
-                startActivity(i);
+                v.postDelayed(() -> {
+                    startActivity(i);
+                    new Timer().schedule(new TimerTask() {
+                        @Override
+                        public void run() {
+                            loginBulto.setVisibility(View.INVISIBLE);
+                        }
+                    }, 1000);
+                },3500);
+
             }else{
                 Toast.makeText(v.getContext(), "Bad credentials", Toast.LENGTH_LONG).show();
             }
